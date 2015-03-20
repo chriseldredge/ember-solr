8bde185fb1d0e3d99bc641125485a8a24cd9f69a
Chris Eldredge <chris.eldredge@gmail.com>
2015-03-20 15:43:42 -0400

    When solrBaseURL is defined and not app-relative, add it to script-src CSP.

--- index.js
+++ index.js
@@ -3,4 +3,24 @@
 
 module.exports = {
   name: 'Solr',
+
+  config: function(environment, appConfig) {
+    if (!appConfig.solrBaseURL || appConfig.solrBaseURL.indexOf(':') === -1) {
+      return;
+    }
+
+    var solrSchemeAndHost = appConfig.solrBaseURL;
+    var solrHostEnd = solrSchemeAndHost.indexOf('/', solrSchemeAndHost.indexOf('://') + 3);
+    if (solrHostEnd > 0) {
+      solrSchemeAndHost = solrSchemeAndHost.substring(0, solrHostEnd);
+    }
+
+    var ENV = {
+      contentSecurityPolicy: {
+        'script-src': "'self' " + solrSchemeAndHost
+      }
+    }
+
+    return ENV;
+  }
 };
