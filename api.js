8e8fa6e3e2e6bcdb33895cc0e17e2c6063fec2fc
Chris Eldredge <chris.eldredge@gmail.com>
2015-03-24 23:07:25 -0400

    yuidocjs

---
+++ api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "SolrAdapter",
+        "SolrHandlerType",
+        "SolrRealTimeGetHandler",
+        "SolrRequest",
+        "SolrRequestHandler",
+        "SolrSearchHandler",
+        "SolrSerializer"
+    ],
+    "modules": [
+        "solr"
+    ],
+    "allModules": [
+        {
+            "displayName": "solr",
+            "name": "solr"
+        }
+    ]
+} };
+});
