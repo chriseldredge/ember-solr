add8993928fd4d044adaecd691b3301119f14bc5
Chris Eldredge <chris.eldredge@gmail.com>
2015-03-31 16:07:18 -0400

    Only include mockjax in development environment.

--- Brocfile.js
+++ Brocfile.js
@@ -5,6 +5,6 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
 
-app.import('bower_components/jquery-mockjax/jquery.mockjax.js');
+app.import({ development: 'bower_components/jquery-mockjax/jquery.mockjax.js'});
 
 module.exports = app.toTree();
